d48bfd1bb974b8bb7b63bff717e2ac0893ace9e3
Geeoon Chung <geeoon13@gmail.com>
2021-09-20 23:45:19 -0500

    Modified to work with any number of digits and it only checks if the previous iteration is the same as the current iteration as to prevent an infinite loop.

--- KaprekarConstant/src/main.cpp
+++ KaprekarConstant/src/main.cpp
@@ -3,44 +3,50 @@
 #include <vector>
 
 int iteration(int num, int numDigits, int i) {
-	std::vector<int> digitList;
+	std::vector<int>* digitList;
+	digitList = new std::vector<int>;
 	for (int i = numDigits; i > 0; i--) {  // stores it in order.
-		digitList.push_back((num / static_cast<int>(pow(10, i - 1))) % 10);
+		digitList->push_back((num / static_cast<int>(pow(10, i - 1))) % 10);
 	}
 	
-	std::sort(digitList.begin(), digitList.end());
+	std::sort(digitList->begin(), digitList->end());
 
 	int descending{ 0 };
 	for (int i = 0; i < numDigits; i++) {
-		descending += digitList[i] * static_cast<int>(pow(10, i));
+		descending += (*digitList)[i] * static_cast<int>(pow(10, i));
 	}
 
 	int ascending{ 0 };
 	for (int i = 0; i < numDigits; i++) {
-		ascending += digitList[i] * static_cast<int>(pow(10, numDigits - i - 1));
+		ascending += (*digitList)[i] * static_cast<int>(pow(10, numDigits - i - 1));
 	}
 
+	digitList->clear();
+	delete digitList;
+
 	int out{ descending - ascending };
-	if (out == 6174) {
-		return i + 1;
+	std::cout << "Iteration " << i << ": " << out << std::endl;
+	if (out == num) {
+		return i;
 	} else {
 		return iteration(out, 4, i + 1);
 	}
 }
 
 int main() {
-	// user input
-	int number{ 0 };
-	std::cout << "Enter a 4 digit number: ";
-	std::cin >> number;
-	while (number < 1 || number > 9999) {
-		std::cout << "Invalid input.  Enter a 4 digit number: ";
+	constexpr int digits{ 8 };
+	while (true) {
+		// user input
+		int number{ 0 };
+		std::cout << "Enter a " << digits << " digit number: ";
 		std::cin >> number;
+		while (number < 1 || number > static_cast<int>(pow(10, digits) - 1)) {
+			std::cout << "Invalid input.  Enter a 4 digit number: ";
+			std::cin >> number;
+		}
+
+		// run the recursive function.
+		std::cout << "It took " << iteration(number, digits, 1) << " iterations." << std::endl;
 	}
-
-	// run the recursive function.
-	std::cout << "It took " << iteration(number, 4, 0) << " iterations.";
-	std::cin >> number;
-
 	return 0;
 }
