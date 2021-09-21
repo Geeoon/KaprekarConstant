4754de3a631208afe979569c6cd9f1a69b2a7b12
Geeoon Chung <geeoon13@gmail.com>
2021-09-21 09:20:15 -0500

    Added maximizer.

--- KaprekarConstant/src/main.cpp
+++ KaprekarConstant/src/main.cpp
@@ -25,7 +25,7 @@ int iteration(int num, int numDigits, int i) {
 	delete digitList;  // clear memory to conserve resources
 
 	int out{ descending - ascending };
-	std::cout << "Iteration " << i << ": " << out << std::endl;
+	//std::cout << "Iteration " << i << ": " << out << std::endl;
 	if (out == num) {  // base case.  This is almost the same as out == 6174; however, this method prevents infinite loops with 0
 		return i;  // return the number of iterations
 	} else {
@@ -34,19 +34,33 @@ int iteration(int num, int numDigits, int i) {
 }
 
 int main() {
-	constexpr int digits{ 4 };
+	constexpr int digits{ 6 };
+	int number{ 0 };
+	/*
+	int max{ 0 };
+	int maxI{ 0 };
+	for (int i = 1; i < pow(10, digits); i++) {
+		int iterations{ iteration(i, digits, 1) };
+		if (iterations > max) {
+			maxI = iterations;
+			max = i;
+		}
+	}
+	std::cout << "The " << digits << " digit number that took the most iterations is " << max << " with " << maxI << " iterations.";
+	*/
 	while (true) {
 		// user input
-		int number{ 0 };
 		std::cout << "Enter a " << digits << " digit number: ";
 		std::cin >> number;
 		while (number < 1 || number > static_cast<int>(pow(10, digits) - 1)) {
-			std::cout << "Invalid input.  Enter a 4 digit number: ";
+			std::cout << "Invalid input.  Enter a " << digits << " digit number: ";
 			std::cin >> number;
 		}
 
 		// run the recursive function.
 		std::cout << "It took " << iteration(number, digits, 1) << " iterations." << std::endl;
 	}
+	int test{ 0 };
+	std::cin >> test;
 	return 0;
 }
